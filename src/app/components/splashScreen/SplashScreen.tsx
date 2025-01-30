8e7d60e983f40fc641610211d9ccd3ebbe827906
Storyos <storyoser@pukyong.ac.kr>
2025-01-30 16:51:31 +0900

    fix: Splash Screen 새로고침에 안뜨게 수정

--- src/app/components/splashScreen/SplashScreen.tsx
+++ src/app/components/splashScreen/SplashScreen.tsx
@@ -3,27 +3,34 @@
 import { useEffect, useState } from "react";
 
 export default function SplashScreen() {
-  const [isVisible, setIsVisible] = useState(true);
-
+  const [isVisible, setIsVisible] = useState<null | boolean>(null);
   useEffect(() => {
-    const hasSeenSplash = sessionStorage.getItem("splashSeen");
-
-    if (hasSeenSplash) {
-      setIsVisible(false); // 첫 실행이 아니면 바로 숨김
+    const navigationEntry = performance.getEntriesByType(
+      "navigation"
+    )[0] as PerformanceNavigationTiming;
+    const isReload = navigationEntry?.type === "reload";
+    if (isReload) {
+      setIsVisible(false);
     } else {
-      setTimeout(() => {
-        sessionStorage.setItem("splashSeen", "true");
+      const hasSeenSplash = sessionStorage.getItem("splashSeen");
+      if (hasSeenSplash) {
         setIsVisible(false);
-      }, 2000); // 2초 후 숨김
+      } else {
+        setTimeout(() => {
+          sessionStorage.setItem("splashSeen", "true");
+          setIsVisible(false);
+        }, 2000);
+        setIsVisible(true);
+      }
     }
   }, []);
 
-  if (!isVisible) return null; // SplashScreen 제거
+  if (isVisible === null || !isVisible) return null;
 
   return (
-    <div className="font-lexend flex-col text-white  font-bold fixed inset-0 flex items-center justify-center bg-[#6A51E6] min-h-screen">
+    <div className="font-lexend flex-col text-white font-bold fixed inset-0 flex items-center justify-center bg-[#6A51E6] min-h-screen">
       <h1 className="text-[64px] items-center tracking-widest">WIKO</h1>
-      <h2 className="text-[15px] animate-fade-in ">한국에 오신걸 환영합니다</h2>
+      <h2 className="text-[15px] animate-fade-in">한국에 오신걸 환영합니다</h2>
       <h3 className="font-light animate-fade-in-delay">Work + Korea</h3>
     </div>
   );
