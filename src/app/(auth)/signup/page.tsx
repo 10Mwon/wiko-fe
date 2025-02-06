d31fe50ac1e35d98adad978e55d15f7d3069a132
Storyos <storyoser@pukyong.ac.kr>
2025-02-06 15:21:51 +0900

    [fix] : 타이틀 padding값 수정

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -19,10 +19,10 @@ export default function Page() {
     address: "",
     visa: "",
   });
-  
+
   return (
     <div className="tracking-[0.1125rem] text-black px-4">
-      <h1 className="text-4xl pt-[4.56rem] text-wikoBlue text-opacity-50 pb-[8.37rem] text-center font-[900]">
+      <h1 className="text-4xl pt-[4.56rem] text-wikoBlue text-opacity-50 pb-[4.12rem] text-center font-[900]">
         WIKO
       </h1>
       {step === 1 && (
