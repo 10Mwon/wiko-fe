ffcbf2e876a7091c738f67daef70f396d8d6f403
Storyos <storyoser@pukyong.ac.kr>
2025-02-14 06:45:05 +0900

    [FEAT] : 자동로그인 생성 및 resume middleware처리

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,14 +2,15 @@ import SignInForm from "@/components/forms/auth/SignInForm";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 
-function Page() {
+function Page({ searchParams }: { searchParams: { [key: string]: string } }) {
   const t = useTranslations("input");
+  const autoLogin = searchParams.autoLogin === "true";
   return (
     <div className="tracking-[0.1125rem] text-white px-4">
       <h1 className="text-4xl pt-[4.56rem] pb-[8.37rem] text-center font-[900]">
         WIKO
       </h1>
-      <SignInForm />
+      <SignInForm autoLogin={autoLogin} />
       <hr className="mt-16" />
       <Link className="flex justify-center pt-4" href={"/signup"}>
         {t("join")}
